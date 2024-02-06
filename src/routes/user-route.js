5cdbc44f160ab7f0eac500cb3a31d551bcecd974
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-02-06 16:40:03 -0500

    Added 405 Method not allowed error for routes other than the routes mentioned in swagger doc

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import * as UserController from '../controllers/user-controller.js';
-import authenticateUser from '../middlewares/authentication.js';
 
 const router = express.Router();
 
 router.route("/v1/user")
-    .post(UserController.createUser);
+    .post(UserController.createUser)
+    .head(UserController.methodNotAllowed)
+    .all(UserController.methodNotAllowed);
 
 router.route("/v1/user/self")
     .get(UserController.getUser)
     .put(UserController.updateUser)
+    .head(UserController.methodNotAllowed)
     .all(UserController.methodNotAllowed);
 
 export default router;
