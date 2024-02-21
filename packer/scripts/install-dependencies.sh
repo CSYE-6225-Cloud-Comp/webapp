06bbb49366d159c9da1bd7aff0106d2cd4f198f6
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-02-21 15:59:17 -0500

    Updated shell scripts and packer file

--- packer/scripts/install-dependencies.sh
+++ packer/scripts/install-dependencies.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
+
+export DBHOST=${DBHOST}
+export DBUSER=${DBUSER}
+export DBPASS=${DBPASS}
+export DATABASE=${DATABASE}
+export PORT=${PORT}
+export DBPORT=${DBPORT}
+
 # Install MySQL server
 sudo dnf install mysql-server -y
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
-mysql -u root -p -e \
+mysql -u ${DBUSER} -p -e \
 "Select user, authentication_string, plugin, host from mysql.user;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
-musql -u root -p -e 
+ALTER USER ${DBUSER}@${DBHOST} IDENTIFIED WITH mysql_native_password BY ${DBPASS};"
+echo -ne '\n'
 
 # Install Nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 nvm install v18.17.1
 
+# Unzip webapp
+cd tmp 
+unzip webapp.zip -d webapp
+
+# Change ownership
+sudo chown -R csye6225:csye6225 webapp
+
+cd webapp
+
 # NPM install
 npm install
 
-# Set ownership of application artifacts and configuration files
-# chown -R csye6225:csye6225 /path/to/application
+sudo cp ./packer/webapp.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
