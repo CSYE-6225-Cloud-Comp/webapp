13aeb82d7727ae5c7f46a8ce93a8094bfa08c863
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-02-21 21:07:27 -0500

    Updated scripts

--- packer/scripts/install-dependencies.sh
+++ packer/scripts/install-dependencies.sh
@@ -12,24 +12,26 @@ sudo dnf install mysql-server -y
 sudo systemctl start mysqld.service
 sudo systemctl status mysqld
 sudo systemctl enable mysqld
-mysql -u root -e "CREATE DATABASE $DATABASE;"
+mysql -u root -e "CREATE DATABASE $DATABASE"
 
 # Install Nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 nvm install v18.17.1
 
-# Unzip webapp
-sudo mv /tmp/webapp.zip /opt
-cd /opt
 sudo yum install unzip
+
+# Unzip webapp
+mkdir -p /opt/application
+sudo mv /tmp/webapp.zip /opt/application/webapp.zip
+cd /opt/application
 unzip webapp.zip -d webapp
 cd /webapp
 npm install
 
 # Change ownership
-sudo chown -R csye6225 webapp
-sudo chgrp -R csye6225 webapp
+sudo chown -R csye6225 application
+sudo chgrp -R csye6225 application
 
 sudo cp ./packer/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
