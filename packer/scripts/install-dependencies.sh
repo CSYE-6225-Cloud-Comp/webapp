dbc9929bb9f5851d35cb6f5d85c08e1cece09567
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-02-22 08:28:03 -0500

    Updated scripts

--- packer/scripts/install-dependencies.sh
+++ packer/scripts/install-dependencies.sh
@@ -7,19 +7,24 @@ export DATABASE=${DATABASE}
 export PORT=${PORT}
 export DBPORT=${DBPORT}
 
-# # Install MySQL server
-# sudo dnf install mysql-server -y
-# sudo systemctl start mysqld.service
-# sudo systemctl status mysqld
-# sudo systemctl enable mysqld
-# sudo mysql_secure_installation << EOF
-# y
-# 2
-# $DBPASS
-# $DBPASS
-# y
-# EOF
-# mysql -u root -p $DBPASS -e "CREATE DATABASE $DATABASE;"
+# Get the repo RPM and install it.
+wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm 
+yum -y install ./mysql57-community-release-el7-7.noarch.rpm 
+
+# Install the server and start it
+yum -y install mysql-community-server 
+systemctl start mysqld 
+
+# Get the temporary password
+temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
+
+# Set up a batch file with the SQL commands
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password@123'; flush privileges;" > reset_pass.sql
+
+# Log in to the server with the temporary password, and pass the SQL file to it.
+mysql -u root --password="$temp_password" --connect-expired-password < reset_pass.sql
+
+mysql -u root -p password@123 -e "CREATE DATABASE $DATABASE;"
 
 # Install Nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -29,12 +34,10 @@ nvm install v18.17.1
 sudo yum install unzip
 
 # Unzip webapp
-sudo mv /tmp/webapp.zip /opt/webapp.zip
-cd /opt
 unzip webapp.zip -d webapp
-cd /webapp
+sudo mv /tmp/webapp /opt/webapp
+cd /opt/webapp
 npm install
-
 # Change ownership
 sudo chown -R csye6225 webapp
 sudo chgrp -R csye6225 webapp
