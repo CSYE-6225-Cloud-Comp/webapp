70a2fff50336284a0896f6cd38a08648a0c880e2
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-03-19 19:20:33 -0400

    Assignment-04

--- logger.js
+++ logger.js
@@ -1,22 +1,24 @@
 import winston from 'winston';
 import { format } from 'winston';
+import moment from 'moment-timezone';
+
+const customFormat = winston.format.printf(({ timestamp, level, message }) => {
+  return JSON.stringify({
+    timestamp: timestamp,
+    level: level.toUpperCase(),
+    severity: winston.config.npm.levels[level],
+    message: message
+  });
+});
+
+moment.tz.setDefault('America/New_York');
 
 const logger = winston.createLogger({
   level: "info",
   format: format.combine(
     format.json(),
-    format.timestamp({
-        format: () => {
-            const now = new Date();
-            const isoString = now.toISOString();
-            // Extract milliseconds and time zone from ISO string
-            const milliseconds = isoString.slice(20, 23);
-            const timeZone = isoString.slice(23);
-            return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}T${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}.${milliseconds}${timeZone}`;
-          }
-          
-    }),
-    format.printf(({ timestamp, level, message }) => `{ ${timestamp} [${level.toUpperCase()}] (${winston.config.npm.levels[level]}) ${message} }`)
+    format.timestamp({format: moment().format('YYYY-MM-DDTHH:mm:ss.SSS')}),
+    customFormat
   ),
   transports: [
     new winston.transports.File({ filename: "/var/log/webapp/webapp.log" }),
