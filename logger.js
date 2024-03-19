a8489779ddac79f59bf39d7cfb4743edd07b3ad0
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-03-19 17:28:22 -0400

    Updated logger file

--- logger.js
+++ logger.js
@@ -14,7 +14,9 @@ const logger = winston.createLogger({
             const timeZone = isoString.slice(23);
             return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}T${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}.${milliseconds}${timeZone}`;
           }
+          
     }),
+    format.printf(({ timestamp, level, message }) => `{ ${timestamp} [${level.toUpperCase()}] (${winston.config.npm.levels[level]}) ${message} }`)
   ),
   transports: [
     new winston.transports.File({ filename: "/var/log/webapp/webapp.log" }),
