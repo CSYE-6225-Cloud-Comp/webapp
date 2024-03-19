9474984f03dbc5af0a36f593f19f7c0464ff29c1
Tanmay Uday Gokhale <gokhale.ta@northeastern.edu>
2024-03-19 17:21:10 -0400

    Changed permissions for /var/log/webapp folder

--- packer/scripts/install-dependencies.sh
+++ packer/scripts/install-dependencies.sh
@@ -42,13 +42,16 @@ sleep 10
 # Move config file (config.yaml) to /etc/google-cloud-ops-agent/
 sudo mv ./packer/configuration/config.yaml /etc/google-cloud-ops-agent/
 
-# Create a directory in /var/log
-sudo mkdir /var/log/webapp
-
 # Change ownership
 # sudo chown csye6225:csye6225 .
 sudo chown -R csye6225 .
 sudo chgrp -R csye6225 .
+
+# Create a directory in /var/log
+sudo mkdir /var/log/webapp
+cd /var/log/webapp
+sudo chown -R csye6225 .
+sudo chgrp -R csye6225 . 
 
 sudo cp ./packer/webapp.service /etc/systemd/system/
 sudo systemctl daemon-reload
